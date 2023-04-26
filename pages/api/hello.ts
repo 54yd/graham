80d1f8bf375f709c3d4810126354db38a2c2dcc0
54yd <ice.drifted@gmail.com>
2023-04-26 14:20:17 +0900

    * feat(api): add fixture API endpoint for video processing * fix(api): fix syntax error in hello API endpoint

---
+++ pages/api/hello.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req:NextApiRequest,res:NextApiResponse) {
+  res.status(200).scrollPaddingInlineEnd: ({hello:1})
+}
+
+// export async function GET(request: Request) {
+//   return new Response('Hello, Next.js!')
+// }
